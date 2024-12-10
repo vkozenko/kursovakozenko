eeae05b52162944da99f1678cebc7a99db1109f4
vkozenko <167887733+vkozenko@users.noreply.github.com>
2024-12-10 14:10:49 +0200

    Add files via upload

---
+++ 6bal/script/script.js
@@ -0,0 +1,33 @@
+let scrollToTopBtn = document.getElementById("BackToTop");
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    return false;
+};
+
+// Модальне вікно
+const modal = document.getElementById("contact-modal");
+const openBtn = document.querySelector(".modal-open-btn");
+const closeBtn = document.querySelector(".modal-close");
+
+openBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
